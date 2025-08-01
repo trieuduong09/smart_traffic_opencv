15c2ca786c863ef6f8544f869032a0d2467c5c27
trieuduong09 <duongnt6865@ut.edu.vn>
2025-08-01 22:45:10 +0700

    add new CV1.1_grayscale.py

--- CV1.1_grayscale.py
+++ CV1.1_grayscale.py
@@ -2,8 +2,7 @@ import cv2
 
 image = cv2.imread('car3.jpg') 
 
-if image is None:
-    print("Error: Could not read the image.")
+while image is None: print("Error: Could not read the image.")
 else:
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Chuyển đổi ảnh sang thang độ xám
     
